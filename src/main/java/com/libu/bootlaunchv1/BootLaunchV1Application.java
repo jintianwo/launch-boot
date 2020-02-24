55933561102a58ee69b45cecdd0f433d209ff8d5
libusheng <libusheng@dangdang.com>
2020-02-24 15:37:46 +0800

    first commit

---
+++ src/main/java/com/libu/bootlaunchv1/BootLaunchV1Application.java
@@ -0,0 +1,13 @@
+package com.libu.bootlaunchv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLaunchV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLaunchV1Application.class, args);
+    }
+
+}
