4430e525c0d51f071d104dad06c1fb073e5b8df8
libusheng <libusheng@dangdang.com>
2020-02-24 15:40:32 +0800

    add 222

--- src/main/java/com/libu/bootlaunchv1/controller/HelloController.java
+++ src/main/java/com/libu/bootlaunchv1/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello world1111111";
+        return "hello world1111111222";
     }
 
 }
